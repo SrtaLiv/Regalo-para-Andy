f205dca886d5307eafbee26dbb5f75866e9c9702
SrtaLiv <104026140+SrtaLiv@users.noreply.github.com>
2023-01-17 17:44:37 -0300

    Add files via upload

---
+++ regaloandy.kt
@@ -0,0 +1,30 @@
+fun main () {
+
+    println("Ingrese un numero del 1 al 18:")
+    val x = readLine()!!.toInt() as Int
+
+    when (x) {
+        0 -> println("El culo te encero")
+        1 -> println("El culo te vacuno")
+        2 -> println("El culo te lanza torpedos")
+        3 -> println("El culo te ves")
+        4 -> println("El culo te idolatro")
+        5 -> println("El culo te trinco")
+        6 -> println("El culo te abanicareis")
+        7 -> println("El culo te compromete")
+        8 -> println("El culo te abrocho")
+        9 -> println("El culo te llueve")
+        10 -> println("El culo te da acidez")
+        11 -> println("El culo te lo hace Ponce")
+        12 -> println("El culo te tose")
+        13 -> println("El culo te florece")
+        14 -> println("El culo te habla morse")
+        15 -> println("El culo te provoco un esguince")
+        16 -> println("El culo te rompeis")
+        17 -> println("El culo te remonta como a un barrilete")
+        18 -> println("El culo te trasnocho")
+
+        else -> println("Tu mami :D")
+
+    }
+}
